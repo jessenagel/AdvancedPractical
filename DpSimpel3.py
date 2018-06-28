495f7b23d35186e311e8b1eb5fd9d1de46289a37
jessenagel <j.m.nagel@student.vu.nl>
2018-06-28 15:49:13 +0200

    Update DpSimpel3.py

--- DpSimpel3.py
+++ DpSimpel3.py
@@ -9,6 +9,7 @@ def f(i,data,new,m):
     xi_2result = np.zeros((m,1))
     xi_1result = np.zeros((m,m))
     xi_0result = np.zeros((m,m,m))
+    path = np.zeros((i,m))
     HL = 255
     BL = 10
     k=2
@@ -20,7 +21,7 @@ def f(i,data,new,m):
     xi_1result[:,:] = np.inf
     xi_1result[n,n] =0
     xi_0result[:,:,:] = np.inf
-    while k <= 11:
+    while k <= i-1:
         for i0 in range (0,m):
             for imin1 in range (0,m):
                 for imin2 in range (0,m):
@@ -53,18 +54,20 @@ def f(i,data,new,m):
         for l in range (0,m-1):
             xi_1index = np.unravel_index(np.argmin(xi_1result[:, l], axis=None),xi_1result[:,l].shape)
             xi_2result[l]= xi_1result[xi_1index[0],l]
+            path[k-2,l] =  xi_1index[0]
             for o in range (0,m-1):
                 xi_2index =np.unravel_index(np.argmin(xi_0result[:,l,o],axis=None),xi_0result[:,l,o].shape)
                 xi_1result[l,o]= xi_0result[xi_2index[0],l,o]
         k = k+1
         # print(result[0])
+    for z in range (0,i-1):
+        new[z,2] = data[z,2] + 30 - path[z,index[0]] *75 / m
     np.savetxt("argmin.csv",new[:,2],delimiter=',')
     np.savetxt("output.csv",xi_0result[9],delimiter=',')
-    return np.min(xi_0result,axis=None)
+    return np.min(xi_0result,axis=None), path[:,index[0]]
 
 
 def c(a):
-
     cost=1
     if a < 0:
         return -a*2*cost
@@ -79,7 +82,7 @@ def minimaliseer(f):
     return 0
 
 def main():
-    m = 40
+    m = 30
     i = 12
     data = np.genfromtxt('datasimpel.csv',delimiter=',')
     new = np.copy(data)
